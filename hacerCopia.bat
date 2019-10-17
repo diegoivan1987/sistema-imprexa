fbfa3fa5791344fda01435abe98700acd764806b
diego_ivan <diegoivan1987@outlook.com>
2019-10-17 17:17:25 -0500

    se contruyo y se hizo lo necesario para volverlo instalador

--- hacerCopia.bat
+++ hacerCopia.bat
@@ -1,5 +1,5 @@
 @echo off
 cd C:\Program Files\MySQL\MySQL Server 8.0\bin
-mysqldump -h 192.168.100.200 -u root -p imprexa2 > C:\Users\Alex\Desktop\respaldoImprexa_%date:~-10,2%%date:~-7,2%%date:~-4,4%.sql
+mysqldump -h 192.168.100.200 -u root -p imprexa2 > C:\Users\Remedios\Desktop\respaldoImprexa_%date:~-10,2%%date:~-7,2%%date:~-4,4%.sql
 pause
 exit
