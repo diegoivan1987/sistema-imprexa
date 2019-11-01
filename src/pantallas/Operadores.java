333f018c6e8688c3c85277a6118300d9e6fdf729
diego_ivan <diegoivan1987@outlook.com>
2019-11-01 16:22:03 -0600

    se hizo establecio la conexion correctamente y se elimino cualquier referencia a la columna maquina en Operadores

--- src/pantallas/Operadores.java
+++ src/pantallas/Operadores.java
@@ -16,8 +16,9 @@ public class Operadores extends javax.swing.JFrame {
     String nombreTabla = "";//guardara el nombre del operador seleccionado en la tabla
     DefaultTableModel modOp;//guardara el modelo de la tabla
     
-    public Operadores() {
+    public Operadores(Connection con) {
         initComponents();
+        this.con = con;
         vaciar();//inicializamos todo en default
         //se deshabilita el boton de modificacion
         btnMod.setEnabled(false);
@@ -35,11 +36,9 @@ public class Operadores extends javax.swing.JFrame {
         tablaOp = new javax.swing.JTable();
         sueldo_48_hrs = new javax.swing.JTextField();
         nombre = new javax.swing.JTextField();
-        listMaquina = new javax.swing.JComboBox();
         listAyudante = new javax.swing.JComboBox();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         checkboxMod = new javax.swing.JCheckBox();
         guardar = new javax.swing.JButton();
@@ -62,30 +61,29 @@ public class Operadores extends javax.swing.JFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
+        tablaOp.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tablaOpMouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(tablaOp);
 
         jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 490, 210));
 
         sueldo_48_hrs.setText("jTextField1");
-        jPanel1.add(sueldo_48_hrs, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 250, -1, -1));
+        jPanel1.add(sueldo_48_hrs, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 300, 100, -1));
 
         nombre.setText("jTextField1");
-        jPanel1.add(nombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 250, 190, -1));
-
-        listMaquina.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "1", "2", "3" }));
-        jPanel1.add(listMaquina, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 300, -1, -1));
+        jPanel1.add(nombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 250, 190, -1));
 
         listAyudante.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "si", "no" }));
         jPanel1.add(listAyudante, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 300, 50, -1));
 
         jLabel1.setText("Nombre:");
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 250, -1, -1));
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 250, -1, -1));
 
         jLabel2.setText("sueldo(por 48 hrs):");
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 250, -1, 20));
-
-        jLabel3.setText("N° maquina en que trabajara:");
-        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 300, -1, -1));
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 300, -1, 20));
 
         jLabel4.setText("¿Será ayudante de impreso?");
         jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 300, -1, -1));
@@ -132,7 +130,6 @@ public class Operadores extends javax.swing.JFrame {
         String nombreSt = nombre.getText();
         float sueldo48 = Float.parseFloat(sueldo_48_hrs.getText());
         float sueldoHr = sueldo48 / 48;//sueldo por hora
-        int maquina = Integer.parseInt(listMaquina.getSelectedItem().toString());
         String ayudanteSt = listAyudante.getSelectedItem().toString();//obtiene si o no
         boolean ayudante = false;
         if(ayudanteSt.equals("si"))//si sera ayudante, asigna true, si no, asigna false
@@ -144,8 +141,7 @@ public class Operadores extends javax.swing.JFrame {
             ayudante = false;
         }
         //datos a guardar
-        String sql = "insert into operador(nombre, sueldo_48_hrs, sueldo_hr, maquina, ayudante)values('"+nombreSt+"',"+sueldo48+","+sueldoHr+","+maquina+","
-                + ""+ayudante+")";
+        String sql = "insert into operadores(nombre, sueldo_48_hrs, sueldo_hr, ayudante)values('"+nombreSt+"',"+sueldo48+","+sueldoHr+","+ayudante+")";
         try
         {
             st = con.createStatement();
@@ -153,7 +149,7 @@ public class Operadores extends javax.swing.JFrame {
             st.close();
             JOptionPane.showMessageDialog(null, "Se guardo el operador");
             vaciar();//se establecen los campos por default
-            llenarTabla();//se actualiza la tabla de operadores
+            actualizarTabla();//se actualiza la tabla de operadores
         }
         catch(SQLException ex)
         {
@@ -167,11 +163,40 @@ public class Operadores extends javax.swing.JFrame {
         this.dispose();
     }//GEN-LAST:event_regresarActionPerformed
 
+    //cuando le dan clic a la tabla
+    private void tablaOpMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tablaOpMouseClicked
+        if(checkboxMod.isEnabled())//si el checkbox de modificar los datos esta activado
+        {
+            mostrarDatosOperador();//se muestran los datos del operador seleccionado
+        }
+    }//GEN-LAST:event_tablaOpMouseClicked
+
+    private void mostrarDatosOperador()
+    {
+        String nombreOLD = modOp.getValueAt(tablaOp.getSelectedRow(), 0).toString();//obtenemos el nombre del operador seleccionado
+        boolean ayudanteOLD;
+        String sql = "select * from operadores where nombre = '"+nombreOLD+"'";
+        try
+        {
+            st = con.createStatement();
+            rs = st.executeQuery(sql);
+            while(rs.next())
+            {
+                nombre.setText(rs.getString("nombre"));
+                sueldo_48_hrs.setText(rs.getString("sueldo_48_hrs").toString());
+                ayudanteOLD = rs.getBoolean("ayudante");
+            }
+        }
+        catch(SQLException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+    
     private void vaciar()//inicializa todo en el default
     {
         nombre.setText("");
         sueldo_48_hrs.setText("");
-        listMaquina.setSelectedIndex(0);
         listAyudante.setSelectedIndex(0);
     }
     
@@ -183,14 +208,14 @@ public class Operadores extends javax.swing.JFrame {
                 return false;
             }    
         };
-        modOp.setColumnIdentifiers(new Object[]{"Nombre","Sueldo x 48 hrs", "N° maquina","Es ayudante"});
+        modOp.setColumnIdentifiers(new Object[]{"Nombre","Sueldo x 48 hrs", "Es ayudante"});
         tablaOp.setModel(modOp);
     }
     
     private void llenarTabla()//llena la tabla de operadores
     {
         String ayudanteSt = "";//servira para mostrar si o no en ayudante
-        String sql = "select * from operador where nombre is not null";
+        String sql = "select * from operadores where nombre is not null";
         try
         {
             st = con.createStatement();
@@ -205,9 +230,40 @@ public class Operadores extends javax.swing.JFrame {
                 {
                     ayudanteSt = "si";
                 }
-                modOp.addRow(new Object[]{rs.getString("nombre"), rs.getString("sueldo_48_hrs"), rs.getString("maquina"), 
-                    ayudanteSt});
+                modOp.addRow(new Object[]{rs.getString("nombre"), rs.getString("sueldo_48_hrs"), ayudanteSt});
             }
+            rs.close();
+            st.close();
+        }
+        catch(SQLException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+    
+    private void actualizarTabla()//hace lo mismo que llenar tabla, pero la vacía al principio
+    {
+        modOp.setRowCount(0);
+        String ayudanteSt = "";//servira para mostrar si o no en ayudante
+        String sql = "select * from operadores where nombre is not null";
+        try
+        {
+            st = con.createStatement();
+            rs = st.executeQuery(sql);
+            while(rs.next())
+            {
+                if(rs.getBoolean("ayudante") == false)
+                {
+                    ayudanteSt = "no";
+                }
+                else if(rs.getBoolean("ayudante") == true)
+                {
+                    ayudanteSt = "si";
+                }
+                modOp.addRow(new Object[]{rs.getString("nombre"), rs.getString("sueldo_48_hrs"), ayudanteSt});
+            }
+            rs.close();
+            st.close();
         }
         catch(SQLException ex)
         {
@@ -221,12 +277,10 @@ public class Operadores extends javax.swing.JFrame {
     private javax.swing.JButton guardar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JComboBox listAyudante;
-    private javax.swing.JComboBox listMaquina;
     private javax.swing.JTextField nombre;
     private javax.swing.JButton regresar;
     private javax.swing.JTextField sueldo_48_hrs;
--- src/pantallas/Principal.java
+++ src/pantallas/Principal.java
@@ -319,7 +319,7 @@ public class Principal extends javax.swing.JFrame {
     }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
-        Operadores ope = new Operadores();
+        Operadores ope = new Operadores(con);
         ope.setVisible(true);
         ope.setLocationRelativeTo(null);
         dispose();
