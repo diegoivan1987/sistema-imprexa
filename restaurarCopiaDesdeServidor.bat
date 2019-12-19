682ee0fd31a63f2b439140d78a52e95ccc0e0e0d
Diego <diegoivan1987@outlook.com>
2019-12-18 19:44:18 -0600

    se corrigio la clase CopiasSeguridad

--- restaurarCopiaDesdeServidor.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-SET /P fecha= Escriba la fecha del archivo(solo los numeros, sin espacios):
-cd C:\Program Files\MySQL\MySQL Server 8.0\bin
-mysql -h 192.168.100.200 -u root -p imprexa2 < C:\Users\Alex\Desktop\respaldoImprexa_%fecha%.sql
-pause
-exit
