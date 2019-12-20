d34043053f473aa8ed62c1442179821c376d27fa
diego_ivan <diegoivan1987@outlook.com>
2019-12-20 17:03:58 -0600

    se configuro el hacerCopia.bat para que haga la copia tanto en el cliente como en el servidor, pero falta probarlo

--- hacerCopia.bat
+++ hacerCopia.bat
@@ -1,5 +1,7 @@
 @echo off
 cd C:\Program Files\MySQL\MySQL Server 8.0\bin
 mysqldump -h 192.168.100.200 -u root -p imprexa2 > C:\Users\Remedios\Desktop\respaldoImprexa_%date:~-10,2%%date:~-7,2%%date:~-4,4%.sql
+copy C:\Users\Remedios\Desktop\respaldoImprexa_%date:~-10,2%%date:~-7,2%%date:~-4,4%.sql \\ALEX-PC\Users\Public\Documents
+popd
 pause
 exit
